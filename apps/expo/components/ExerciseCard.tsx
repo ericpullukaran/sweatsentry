c5767e410c80a57bfb41ceb42d7510d4ad37a553
Eric Paul <ericcpaul00@gmail.com>
2023-04-11 08:10:42 +1000

    changed styling abit

--- apps/expo/components/ExerciseCard.tsx
+++ apps/expo/components/ExerciseCard.tsx
@@ -3,38 +3,35 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 function ExerciseCard() {
   return (
-    <View className="rounded-xl bg-gray-300">
-      <TouchableOpacity className="m-3 mb-4 flex h-16 flex-row ">
+    <View className="rounded-xl bg-orange-300">
+      <TouchableOpacity className="z-40 flex flex-row rounded-lg bg-slate-400 p-3">
         <View className=" mr-2 w-20 rounded-md bg-red-300"></View>
         <View className="">
           <Text className="text-lg font-extrabold">Chest</Text>
           <Text>asdf</Text>
         </View>
       </TouchableOpacity>
-      <View className="justify-center rounded-t-lg bg-gray-500 px-3 py-4">
+      <View className="-top-4 justify-center rounded-lg bg-base-100 px-3 py-4 pt-7">
         <Text className="text-md font-medium">Set 1</Text>
         <View className="mt-1 flex-row gap-4">
           <View>
             <Text className="text-lg font-extrabold">Weight</Text>
             <TextInput
               keyboardType="numeric"
-              className="h-8 rounded-lg bg-white"
+              className="h-8 rounded-lg border-2 border-black/70"
             ></TextInput>
           </View>
           <View>
             <Text className="text-lg font-extrabold">Reps</Text>
             <TextInput
               keyboardType="numeric"
-              className="h-8 rounded-lg bg-white"
+              className="h-8 rounded-lg border-2 border-black/70"
             ></TextInput>
           </View>
         </View>
       </View>
-      <View className="h-12 justify-center  bg-gray-400 px-3">
-        <Text className="text-md font-extrabold">Set 2</Text>
-      </View>
-      <View className="h-12 justify-center rounded-b-lg bg-gray-500 px-3">
-        <Text className="text-md font-extrabold">Set 1</Text>
+      <View className="items-center pb-3">
+        <Text className="text-lg font-extrabold">Add Set</Text>
       </View>
     </View>
   );
