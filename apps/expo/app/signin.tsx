a88eb0bad0c06da2326b2a942f38bb8b8397df9b
Eric Paul <ericcpaul00@gmail.com>
2023-04-10 19:22:17 +1000

    fixed tailwind

--- apps/expo/app/signin.tsx
+++ apps/expo/app/signin.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 
-import { View, Image } from "react-native";
+import { View, Image, ScrollView } from "react-native";
 
 import SignInWithOAuth from "~/components/SignInWithOAuth";
 
 export const SignInSignUpScreen = () => {
   return (
-    <>
+    <ScrollView>
       <View className="mx-8">
-        <View className="mt-[35%] flex flex-row justify-center">
+        <View className="mt-[15%] flex flex-row justify-center bg-success">
           <Image
             source={require("../assets/aa_blob.png")}
             style={{
               resizeMode: "contain",
-              width: "100%",
             }}
             className="w-full"
           />
         </View>
       </View>
-      <View className="h-full p-4">
+      <View className="p-4">
         <SignInWithOAuth />
       </View>
-    </>
+    </ScrollView>
   );
 };
