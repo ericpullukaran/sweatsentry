7cfa163ceabdc25cf2d27ede1acbec5af0360edc
Eric Paul <ericcpaul00@gmail.com>
2023-04-10 19:42:39 +1000

    small styling changes

--- apps/expo/components/SignInWithOAuth.tsx
+++ apps/expo/components/SignInWithOAuth.tsx
@@ -42,16 +42,15 @@ const SignInWithOAuth = () => {
   return (
     <>
       <Text className="p-4 text-center text-4xl">
-        <Text className="text-orange-400 font-extrabold">Ace</Text> your fitness
+        <Text className="font-extrabold text-orange-400">Ace</Text> your fitness
         goals
       </Text>
-      <View className="mx-auto mt-4 mb-8 w-[75%] flex-row items-center">
-        <View className="h-[1px] flex-1 bg-[#000000]" />
+      <View className="mx-auto mb-8 mt-4 w-[75%] flex-row items-center">
+        <View className="h-[1px] flex-1 bg-black" />
         <View className="mx-3">
           <UserIcon color={"black"} width={20} />
         </View>
-        {/* XX IDK WHY I CANT DO BG-BLACK only bg-[#000000] works... we also need the primary/secondary stuff to work */}
-        <View className="h-[1px] flex-1 bg-[#000000]" />
+        <View className="h-[1px] flex-1 bg-black" />
       </View>
 
       <View className="flex-row justify-center gap-4">
